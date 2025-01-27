0ed561e4aeb0f2a0ef9232568576fbf65b20bd86
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-01-27 10:52:36 +0100

    First commit

---
+++ src/test/java/com/salesianostriana/dam/satapp/SatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.satapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
